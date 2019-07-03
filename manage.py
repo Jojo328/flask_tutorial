ecd971ce1499ffb1a5acda2a325c911ba183c7f3
Armand DEBORDE <armand.deborde@gmail.com>
2019-07-03 11:07:35 +0200

    changed some db stuff about manage.py

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from flaskr import create_app
+from flaskr import db
 
-migrate = Migrate(create_app())
+migrate = Migrate(create_app(), db)
 
 manager = Manager(create_app())
 manager.add_command('db', MigrateCommand)
